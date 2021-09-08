380ab2025795b74168c6df691024701ed68d25e2
Anshul Rajput <anshulrajput@Anshuls-MacBook-Air.local>
2021-09-08 20:50:44 +0530

    Added template functionality

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'title': 'Fire1',
+        'author': 'Anshul',
+        'content': 'This is fire1.',
+        'date_posted': '8-sept-2021'
+    },
+    {
+        'title': 'Fire2',
+        'author': 'Rahul',
+        'content': 'This is fire2.',
+        'date_posted': '7-sept-2021'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
